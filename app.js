ef0acf26747a164821e4d2e37fec2444e1cab49e
Nikita Mamaev <palmally@yandex.ru>
2022-10-29 18:16:59 +0300

    feat: add winston logger

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const { errors: validationErrorsHandler } = require('celebrate');
 const router = require('./routes/index');
 const errorsHandler = require('./middlewares/errorsHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, NODE_ENV } = process.env;
 
@@ -12,8 +13,10 @@ const app = express();
 db.connect('mongodb://localhost:27017/bitfilmsdb');
 app.use(express.json());
 app.use(cookieParser());
+app.use(requestLogger);
 app.use(router);
 
+app.use(errorLogger);
 app.use(validationErrorsHandler());
 app.use(errorsHandler);
 
